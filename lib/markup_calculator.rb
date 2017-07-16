8177680f046e96e127a7ae9a2b3727be71ce53c6
Meep <mwraich@financeit.io>
2017-07-16 19:23:34 -0400

    correct indenting in calculator model, clean up code from comments

--- lib/markup_calculator.rb
+++ lib/markup_calculator.rb
@@ -1,47 +1,32 @@
 class MarkupCalculator
-attr_reader :worker_count, :material_type
-attr_accessor :flat_markup
+  attr_reader :worker_count, :material_type
+  attr_accessor :flat_markup
 
-FLAT_MARKUP = 0.05.freeze
-WORKER_MARKUP = 0.012.freeze
-MATERIAL_MARKUP = {drugs: 0.075, food: 0.13, electronics: 0.02}
+  FLAT_MARKUP = 0.05.freeze
+  WORKER_MARKUP = 0.012.freeze
+  MATERIAL_MARKUP = {drugs: 0.075, food: 0.13, electronics: 0.02}
 
-def initialize(base_price, worker_count, material_type)
-  @flat_markup = format_flat_markup(base_price)
-  @worker_count = worker_count.split.first.to_i
-  @material_type = material_type
-end
+  def initialize(base_price, worker_count, material_type)
+    @flat_markup = format_flat_markup(base_price)
+    @worker_count = worker_count.split.first.to_i
+    @material_type = material_type
+  end
 
-def format_flat_markup(base_price)
-  formated_base_price = base_price.delete('$,').to_f
-  formated_base_price + (formated_base_price * FLAT_MARKUP)
-end
+  def format_flat_markup(base_price)
+    formated_base_price = base_price.delete('$,').to_f
+    formated_base_price + (formated_base_price * FLAT_MARKUP)
+  end
 
-def worker_markup
-  flat_markup * (worker_count * WORKER_MARKUP)
-end
+  def worker_markup
+    flat_markup * (worker_count * WORKER_MARKUP) 
+  end
 
-def material_markup
-  MATERIAL_MARKUP[material_type.to_sym] ? (flat_markup * MATERIAL_MARKUP[material_type.to_sym]) : 0
-end
+  def material_markup
+    MATERIAL_MARKUP[material_type.to_sym] ? (flat_markup * MATERIAL_MARKUP[material_type.to_sym]) : 0
+  end
 
-def total
-  total = (flat_markup + worker_markup + material_markup).round(2)
-  "$#{total.to_s.gsub(/(\d)(?=\d{3}+(\.\d*)?$)/, '\1,')}"
-end
-
-# Without exception, there is a flat markup on all jobs of 5%
-# For each person that needs to work on the job, there is a markup of 1.2%
-
-# materials
-
-# If pharmaceuticals are involved, there is an immediate 7.5% markup
-# For food, there is a 13% markup
-# Electronics require a 2% markup
-# Everything else, there is no markup
-
-# Another system calculates the base price depending on how many products need to be repackaged. As such, the markup calculator should accept the initial base price along with the different categories of markups and calculate a final cost for a project.
-
-# The flat markup is calculated first and then all other markups are calculated on top of the base price plus flat markup.
-
+  def total
+    total = (flat_markup + worker_markup + material_markup).round(2)
+    "$#{total.to_s.gsub(/(\d)(?=\d{3}+(\.\d*)?$)/, '\1,')}"
+  end
 end
--- spec/markup_calculator_spec.rb
+++ spec/markup_calculator_spec.rb
@@ -48,15 +48,3 @@ describe MarkupCalculator do
     end
   end
 end
-
-#1
-# Input: $1,299.99, 3 people, food
-# Output: $1,591.58
-
-#2
-# Input: $5,432.00, 1 person, drugs
-# Output: $6,199.81
-
-# 3
-# Input: $12,456.95, 4 people, books
-# Output: $13,707.63
