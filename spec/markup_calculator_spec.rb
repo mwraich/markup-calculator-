a59f8a2ac529133cc7f98910904491e63c7c0b2d
Meep <mwraich@financeit.io>
2017-07-16 17:45:13 -0400

    Set up spec for initializing markup calculator

---
+++ spec/markup_calculator_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../lib/markup_calculator'
+
+describe MarkupCalculator do
+  it 'accepts a base price, number of workers and material type' do
+    calc = MarkupCalculator.new('$1,299.99', '3 people', 'food')
+    expect(calc.base_price).to eq('$1,299.99')
+    expect(calc.worker_count).to eq('3 people')
+    expect(calc.material_type).to eq('food')
+  end
+end
+
+#1
+# Input: $1,299.99, 3 people, food
+# Output: $1,591.58
+
+#2
+# Input: $5,432.00, 1 person, drugs
+# Output: $6,199.81
+
+# 3
+# Input: $12,456.95, 4 people, books
+# Output: $13,707.63
