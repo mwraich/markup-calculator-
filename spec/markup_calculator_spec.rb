a1ba084fab7f3db82904c36cdf60daa1bca48904
Meep <mwraich@financeit.io>
2017-07-16 18:21:06 -0400

    reformat specs, generate spec for drug material mark up

--- spec/markup_calculator_spec.rb
+++ spec/markup_calculator_spec.rb
@@ -1,16 +1,25 @@
 require_relative '../lib/markup_calculator'
 
 describe MarkupCalculator do
-  it 'accepts a base price, number of workers and material type' do
-    calc = MarkupCalculator.new('$1,299.99', '3 people', 'food')
-    expect(calc.flat_markup).to eq(1299.99 + (1299.99 * MarkupCalculator::FLAT_MARKUP))
-    expect(calc.worker_count).to eq(3)
-    expect(calc.material_type).to eq('food')
+  context 'flat markup price' do
+    it 'accepts a base price, number of workers and material type' do
+      calc = MarkupCalculator.new('$1,299.99', '3 people', 'food')
+      expect(calc.flat_markup).to eq(1299.99 + (1299.99 * MarkupCalculator::FLAT_MARKUP))
+      expect(calc.worker_count).to eq(3)
+      expect(calc.material_type).to eq('food')
+    end
   end
-
-  it 'has a worker markup of 1.2% per person' do
-    calc = MarkupCalculator.new('$1,299.99', '3 people', 'food')
-    expect(calc.worker_markup).to eq(calc.flat_markup * (3 * MarkupCalculator::WORKER_MARKUP))
+  context '#worker_markup' do
+    it 'has a worker markup of 1.2% per person' do
+      calc = MarkupCalculator.new('$1,299.99', '3 people', 'food')
+      expect(calc.worker_markup).to eq(calc.flat_markup * (3 * MarkupCalculator::WORKER_MARKUP))
+    end
+  end
+  context '#material_markup' do
+    it 'has a 13% markup for drugs' do
+      calc = MarkupCalculator.new('$1,299.99', '3 people', 'drugs')
+      expect(calc.material_markup).to eq(calc.flat_markup * 0.075)
+    end
   end
 end
 
